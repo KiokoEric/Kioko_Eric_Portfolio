e6604659552c618a22b61d470ebb719e8d44ecd4
[KiokoEric] <[kiokoerick040@gmail.com]>
2023-11-14 09:00:10 +0300

    Correction of Home Page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -15,7 +15,7 @@ return (
         <section>
             <h2>Hello, My Name is Eric Kioko <span className='Greet' ><MdWavingHand size="1.8rem"/></span></h2>
             <p>
-                I am a Fullstack Web Developer based in Nairobi, Kenya. I am a passionate and enthusiastic software developer with a passion for crafting clean, efficient, and user-friendly software solutions.
+                I am a Front End Web Developer based in Nairobi, Kenya. I am a passionate and enthusiastic software developer with a passion for crafting clean, efficient, and user-friendly software solutions.
             </p>
             <div>
                 <a href='#Contact'><button>Contact Now</button></a>
